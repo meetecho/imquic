6d6c622358ae122689c4133ef1878079c8c864a2
Lorenzo Miniero <lminiero@gmail.com>
2026-09-09 15:04:45 +0200

    Advertise MoQ v21 support

--- src/imquic-moq.c
+++ src/imquic-moq.c
@@ -1063,8 +1063,10 @@ const char *imquic_moq_version_str(imquic_moq_version version) {
 			return "draft-ietf-moq-transport-19";
 		case IMQUIC_MOQ_VERSION_20:
 			return "draft-ietf-moq-transport-20";
+		case IMQUIC_MOQ_VERSION_21:
+			return "draft-ietf-moq-transport-21";
 		case IMQUIC_MOQ_VERSION_ANY:
-			return "draft-ietf-moq-transport-XX(-from--16-to-20)";
+			return "draft-ietf-moq-transport-XX(-from--16-to-21)";
 		default: break;
 	}
 	return NULL;
@@ -1082,8 +1084,10 @@ static const char *imquic_moq_version_alpn(imquic_moq_version version) {
 			return "moqt-19";
 		case IMQUIC_MOQ_VERSION_20:
 			return "moqt-20";
+		case IMQUIC_MOQ_VERSION_21:
+			return "moqt-21";
 		case IMQUIC_MOQ_VERSION_ANY:
-			return "moqt-20,moqt-19,moqt-18,moqt-17,moqt-16";
+			return "moqt-21,moqt-20,moqt-19,moqt-18,moqt-17,moqt-16";
 		default: break;
 	}
 	return NULL;
